e226998d373bd4739d8e764dd66ea95503cbb587
K.Siniakov <k.siniakov@gmail.com>
2022-05-19 12:25:23 +0300

    Stage 1

---
+++ Bulls and Cows/task/src/bullscows/model/Game.java
@@ -0,0 +1,109 @@
+package bullscows.model;
+
+import java.util.Arrays;
+
+public class Game {
+    public static final int codeSize = 4;
+    char[] code;
+    int count;
+
+    public Game(){
+        code = new char[codeSize];
+        count = 1;
+    }
+
+    public void initGame() {
+        code = new char[] {'9', '3', '0', '5'};
+        sayCodePrepared();
+    }
+
+    public String toString() {
+        String result = "";
+        for (int i = 0; i < codeSize; i++) {
+            result += code[i];
+        }
+        return result;
+    }
+    void incCount() {
+        count++;
+    }
+    public void sayTurn() {
+        System.out.println("\nTurn " + count + ". Answer:");
+
+    }
+
+    void sayCodePrepared() {
+        System.out.println("The secret code is prepared: ****.");
+    }
+
+    void sayWin() {
+        System.out.println("Congrats! The secret code is " + toString() + ".");
+    }
+
+    char[] convert(int answer) {
+        char[] res = new char[codeSize];
+        for (int i = 0; i < codeSize; i++) {
+            int tmp = 1;
+            for (int j = 1; j < codeSize - i; j++) {
+                tmp *= 10;
+            }
+            res[i] = (char)(((answer / tmp) %  10 ) + 48);
+        }
+        return res;
+    }
+
+    int getBulls(int answer) {
+        int result = 0;
+        char[] buffer = convert(answer);
+        for (int i = 0; i < codeSize; i++) {
+            if (buffer[i] == code[i]) {
+                result++;
+            }
+        }
+        return result;
+    }
+
+    int getCows(int answer) {
+        int result = 0;
+        char[] buffer = convert(answer);
+        for (int i = 0; i < codeSize; i++) {
+            for (int j = 0; j < codeSize; j++) {
+                if (buffer[i] == code[j] && i != j) {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
+    public boolean processAnswer(int answer) {
+        int cows = getCows(answer);
+        int bulls = getBulls(answer);
+
+        String result = "Grade: ";
+        String bullsStr = "";
+        String cowsStr = "";
+
+        incCount();
+
+        if (bulls == 0 && cows == 0) {
+            result = result + "None";
+        } else {
+            if (bulls > 0) {
+                bullsStr = bulls + (bulls == 1 ? " bull" : " bulls");
+            }
+            if (cows > 0) {
+                cowsStr = cows + (cows == 1 ? " cow" : " cows");
+            }
+            result = result + bullsStr + (bulls > 0 && cows > 0 ? " and ": "") + cowsStr;
+        }
+
+        result += ".";
+        System.out.println(result);
+        if (bulls == codeSize) {
+            sayWin();
+            return false;
+        }
+        return true;
+    }
+}
